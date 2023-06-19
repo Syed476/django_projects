fe2edad38c12dcfa0c7f2402edc136286eeb2a91
bbk-pij-77 <sali33@mail.bbk.ac.uk>
2023-06-19 15:58:59 +0100

    styling added to the project

--- personal_portfolio-project/portfolio/models.py
+++ personal_portfolio-project/portfolio/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Project (models.Model):
-    title=models.CharField(max_length=10)
+    title=models.CharField(max_length=50)
     description=models.CharField(max_length=120)
     images=models.ImageField(upload_to='portfolio/images')
     url=models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
