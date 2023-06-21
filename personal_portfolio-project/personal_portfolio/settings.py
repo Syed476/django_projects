e60ad4b98812e750e3a3e6c1ccdc7ba08934494f
bbk-pij-77 <sali33@mail.bbk.ac.uk>
2023-06-21 09:07:05 +0100

    debug changed to true

--- personal_portfolio-project/personal_portfolio/settings.py
+++ personal_portfolio-project/personal_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#21(!1bw9=*)=z+7*40k#u-f0b(9e2e87kq+bn!oc3d*kvv#8*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['shah476.pythonanywhere.com']
 
