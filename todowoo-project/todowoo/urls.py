1e0e29862423ee97059f2ae3e89e3584126e51ac
bbk-pij-77 <sali33@mail.bbk.ac.uk>
2023-07-05 19:24:23 +0100

    todo project updated

--- todowoo-project/todowoo/urls.py
+++ todowoo-project/todowoo/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('signup/', views.signupuser,name='signupuser'),
-
+    path('login/', views.loginuser,name='loginuser'),
+    path('logout/', views.logoutuser,name='logoutuser'),
+    path('', views.home,name='home'),
     path('current/', views.currenttodos,name='currenttodos'),
 ]
